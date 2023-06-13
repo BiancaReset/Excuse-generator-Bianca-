d45fbbcc2ddd6d7403423d2cdab6ba9a83b9a53b
BiancaReset <130721122+BiancaReset@users.noreply.github.com>
2023-06-13 01:13:59 +0000

    Versión final Bianca

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["My computer", "The waiter", "My watch", "The car"];
+let action = ["fell", "stole", "loosened", "caught fire"];
+let what = [
+  "inside the pool",
+  "money from the guests",
+  "and dissapeared",
+  "and exploded"
+];
+let when = [
+  "before the class",
+  "on Sunday night",
+  "when I finished",
+  "during my skuba lessons",
+  "in the highway"
+];
+function getRandomInt(max) {
+  let random = Math.random() * max;
+  let randomInt = Math.floor(random);
+  return randomInt;
+}
+let randomWho = getRandomInt(who.length);
+let randomAction = getRandomInt(action.length);
+let randomWhat = getRandomInt(what.length);
+let randomWhen = getRandomInt(when.length);
+console.log(
+  `${who[randomWho]} ${action[randomAction]}, ${what[randomWhat]}, ${when[randomWhen]}`
+);
+document.getElementById(
+  "excuse"
+).innerHTML = `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
